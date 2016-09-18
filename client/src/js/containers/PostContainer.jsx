d52e1b98793971624f75d56b47a7299ea967c091
Ashley Williams <hi@ashleyw.co.uk>
2016-09-18 23:14:24 +0100

    Improved styles

--- client/src/js/containers/PostContainer.jsx
+++ client/src/js/containers/PostContainer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ReactMarkdown from 'react-markdown'
 import TimeAgo from 'react-timeago'
 import { connect, dispatch } from 'react-redux'
+import { Button } from 'react-bootstrap'
 
 import { deletePost } from '../actions/PostsActions'
 
@@ -26,7 +27,7 @@ export default class PostContainer extends Component {
       <article className='post'>
         <h1>{post.title}</h1>
         <TimeAgo date={post.publishedAt * 1000} minPeriod={60} />
-        <button onClick={ this.deletePost.bind(this) }>delete</button>
+        <Button className="delete" onClick={ this.deletePost.bind(this) }>delete</Button>
         <ReactMarkdown source={post.text} />
       </article>
     );
